57a6f67fa13547b8e9911f8e6557d069468b6ee9
Giovanni <42092549+Matocolotoe@users.noreply.github.com>
2021-08-28 12:54:35 +0200

    Fix NoSuchMethodError with BoatData

--- src/main/java/ch/njol/skript/entity/BoatData.java
+++ src/main/java/ch/njol/skript/entity/BoatData.java
@@ -55,7 +55,7 @@ public class BoatData extends EntityData<Boat> {
 	@Override
 	protected boolean init(@Nullable Class<? extends Boat> c, @Nullable Boat e) {
 		if (e != null)
-			matchedPattern = 2 + e.getWoodType().ordinal();
+			matchedPattern = 2;
 		return true;
 	}
 
